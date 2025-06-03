8f17bc167886b54e96eb88d32696876ee412bcb8
Ignacia Méndez <87282404+ign-mendezs@users.noreply.github.com>
2025-06-03 17:38:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ const palabras = [
   { text: "RESERVA",     row: 1,  col: 2,  dir: "D" }, // pista 1
   { text: "LA",          row: 6,  col: 4,  dir: "D" }, // pista 2
   { text: "FECHA",       row: 2,  col: 1,  dir: "A" }, // pista 3
-  { text: "VEINTISIETE", row: 1,  col: 7,  dir: "D" }, // pista 4
+  { text: "VEINTISIETE", row: 1,  col: 8,  dir: "D" }, // pista 4
   { text: "DICIEMBRE",   row: 11, col: 3,  dir: "A" }, // pista 5
-  { text: "NOS",         row: 7,  col: 6,  dir: "D" }  // pista 6
+  { text: "NOS",         row: 6,  col: 6,  dir: "D" }  // pista 6
 ];
 
 // 2. Definimos el tamaño total del crucigrama: 11 filas x 11 columnas
